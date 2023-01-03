dffb22c7329246dcfd06a5db67a35e56c6a7b682
Lídia Garcia <lidiagramosgramos@gmail.com>
2023-01-03 10:56:08 -0300

    first commit

---
+++ Calculadora/Operadorescs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{ 
+    class Operadorescs
+    {
+
+        public double num1;
+        public double num2;
+        public double result;
+
+        public void mais(double a, double b)
+        { 
+            num1 = a;
+            num2 = b;
+            result = num1 + num2;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        public void menos(double a, double b)
+        {
+            num1 = a;
+            num2 = b;
+            result = num1 - num2;
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+        public void vezes(double a, double b)
+        {
+            
+            num1 = a;
+            num2 = b;
+            result = num1 / num2;
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+        public void dividir(double a, double b)
+        {          
+            num1 = a;
+            num2 = b;
+            result = num1 * num2;
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Operadorescs C = new Operadorescs();
+
+            string menu;
+            
+
+            Console.WriteLine("Digite + para adição;");
+            Console.WriteLine("Digite - para subtração;");
+            Console.WriteLine("Digite * para multiplicação;");
+            Console.WriteLine("Digite / para divisão;");
+            Console.WriteLine("Ou digite Q para fechar o programa.");
+            menu = Console.ReadLine();
+
+
+            if(menu == "+")
+            {
+                Console.WriteLine("Digite os números para realizar a soma");
+                C.mais(double.Parse(Console.ReadLine()),double.Parse(Console.ReadLine()));
+            }
+            else if(menu == "-")
+            {
+                Console.WriteLine("Digite os números para realizar a subtração");
+                C.menos(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+            }
+            else if (menu == "*")
+            {
+                Console.WriteLine("Digite os números para realizar a multiplicação");
+                C.vezes(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+            }
+            else if (menu == "/")
+            {
+                Console.WriteLine("Digite os numeros para realizar a divisão");
+                C.dividir(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+            }
+            else if (menu == "Q")
+            {
+                Environment.Exit(0);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
